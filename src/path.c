ac9e0afd2e150d40a96b2b44be1964f6e72867db
TheOGeniot <jude.guehl@efrei.net>
2024-11-21 13:28:34 +0100

    Initial commit

---
+++ src/path.c
@@ -0,0 +1,5 @@
+//
+// Created by judeg on 21/11/2024.
+//
+
+#include "path.h"
---
+++ src/path.h
@@ -0,0 +1,8 @@
+//
+// Created by judeg on 21/11/2024.
+//
+
+#ifndef C_PROJECT_MARS_ROVER_PATH_H
+#define C_PROJECT_MARS_ROVER_PATH_H
+
+#endif //C_PROJECT_MARS_ROVER_PATH_H
