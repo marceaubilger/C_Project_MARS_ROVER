784c0936ee83296977ddbbc459ee679078b0db66
TheOGeniot <jude.guehl@efrei.net>
2024-11-21 15:46:48 +0100

    Path library in order to find the shortest path for MARC using an N-Ary tree for the possibilities

--- src/path.c
+++ src/path.c
@@ -1,5 +1,45 @@
 //
 // Created by judeg on 21/11/2024.
 //
-
 #include "path.h"
+
+path createPath(int size){
+    path p;
+    p.stack = createStack(size);
+    p.cost = 0;
+    return p;
+}
+
+path find_shortest_path(p_tree tree){
+    path p = createPath(9);
+    p_node node = tree->root;
+    p= find_shortest_path_rec(node,p);
+    return p;
+};
+
+path find_shortest_path_rec(p_node node, path p){
+    if(node==NULL){
+        return p;
+    }
+    int nb_moves_left = 9 - node->height;
+    path *paths = malloc(nb_moves_left*sizeof(path));
+    for(int i=0;i<nb_moves_left;i++){
+        paths[i] = createPath(nb_moves_left);
+        paths[i] = find_shortest_path_rec(node->sons[i],paths[i]);
+    }
+    p=min_path(paths,nb_moves_left);
+    push(&p.stack,node->move_to_get_here);
+    p.cost+=node->value;
+    free(paths);
+    return p;
+}
+
+path min_path(path *paths, int size){
+    path min = paths[0];
+    for(int i=1;i<size;i++){
+        if(paths[i].stack.size<min.stack.size && paths[i].cost<=min.cost){
+            min = paths[i];
+        }
+    }
+    return min;
+}
--- src/path.h
+++ src/path.h
@@ -1,8 +1,45 @@
 //
 // Created by judeg on 21/11/2024.
 //
-
 #ifndef C_PROJECT_MARS_ROVER_PATH_H
 #define C_PROJECT_MARS_ROVER_PATH_H
+#include "N-ary-tree.h"
+#include "stack.h"
+
+struct path{
+    t_stack stack;
+    int cost;
+}typedef path, *p_path;
+
+
+/**
+* @brief Function to create a path
+* @param size : the size of the path
+* @return empty path
+*/
+path CreatePath(int );
+
+/**
+* @brief Function to find the shortest path
+* @param tree : the tree
+* @return the shortest path
+*/
+path findShortestPath(p_tree);
+
+/**
+ * @brief Function to find the shortest path recursively from a starting node
+ * @param node : the starting node
+ * @param p : the path for recursion
+ * @return
+ */
+path find_shortest_path_rec(p_node, path);
+
+/**
+ * @brief Function to find the best path from a list of paths, either shortest or cheapest
+ * @param paths : the list of paths
+ * @param size : the size of the list
+ * @return the shortest path
+ */
+path min_path(path *, int);
 
 #endif //C_PROJECT_MARS_ROVER_PATH_H
